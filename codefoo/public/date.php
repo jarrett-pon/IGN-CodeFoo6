b048bf16ec807376cec9de3abfa68c3101a44862
jtpty45434 <jarrett.pon@gmail.com>
2016-04-19 19:42:55 -0500

    Added files via upload

---
+++ codefoo/public/date.php
@@ -0,0 +1,19 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+    render("date_form.php", ["title"=>"ISO 8601"]);
+    }
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $date = "";
+        $date = $_POST["date"];
+        $outputdate = convertdate($date);
+        render("date_form.php", ["outputdate"=>$outputdate, "title" => "ISO 8601 Results"]);
+    }
+
+?>
